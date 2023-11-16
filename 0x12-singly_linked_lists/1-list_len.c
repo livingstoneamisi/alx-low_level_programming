f1a0d81aa4df7699ec9fd86827ccd834a3d81a45
livingstoneamisi <livingstoneamisi00@gmail.com>
2023-11-15 21:37:59 -0800

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - calculte the number of elements in a linked list.
+ * @h: head of the liked list.
  *
- * Return: number of elements in h
+ * Return: number of elements in the linked list.
  */
+
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t count = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
